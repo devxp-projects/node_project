422a7a4268f1c64319dd93a0886790fda764e91c
Ebukaar <Ebukaar@gmail.com>
2023-04-11 19:27:16 +0100

    first web socket connection try

--- ch4/src/main.ts
+++ ch4/src/main.ts
@@ -1,9 +1,33 @@
 import express from 'express';
+import { WebSocketServer } from 'ws';
+import http from 'http';
+import * as client from "./client";
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3333;
+const port = process.env.PORT ? Number(process.env.PORT) : 3334;
 
 const app = express();
+const server = http.createServer(app); // Create a HTTP server with your Express app
+
+const wss = new WebSocketServer({ server });
+
+
+wss.on('connection', function connection(ws) {
+  console.log('Client connected'); // Log when a new connection is established
+
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
+
+// Add this line to log when a message is sent to the clients
+wss.on('message', (data) => {
+  console.log(`Sent message to clients: ${data}`);
+});
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
@@ -12,3 +36,5 @@ app.get('/', (req, res) => {
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
+
+console.log(client)
