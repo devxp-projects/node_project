5aabd88f5c3c54ce9da8c172fde799725023c856
Ebukaar <Ebukaar@gmail.com>
2023-05-13 12:06:30 +0100

    client authentication across board

--- ch4/src/main.ts
+++ ch4/src/main.ts
@@ -1,29 +1,48 @@
 import express from 'express';
 import { WebSocketServer } from 'ws';
 import http from 'http';
+import { v4 as uuidv4 } from 'uuid';
+import WebSocket from 'ws';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3333;
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocketServer({ server })
+const wss = new WebSocketServer({ server });
 
+interface CustomWebSocket extends WebSocket {
+  dispatchEvent: (event: Event) => boolean;
+}
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
-        wss.clients.forEach(function each(client) {
-            if (client != ws && client.readyState == ws.OPEN) {
-                client.send(data)
-            }
-        })
-    });
-})  
+interface User {
+  username: string;
+  uuid: string;
+  ws: CustomWebSocket;
+}
 
+const users: User[] = [];
 
-// app.get('/', (req, res) => {
-//   res.send({ message: 'Hello API' });
-// });
+wss.on('connection', function connection(ws: CustomWebSocket) {
+  ws.on('message', function incoming(data) {
+    const message = JSON.parse(data.toString());
+    if (message.type === 'auth') {
+      const newUser: User = {
+        username: message.username,
+        uuid: uuidv4(),
+        ws: ws,
+      };
+      users.push(newUser);
+      ws.send(JSON.stringify({ type: 'auth', uuid: newUser.uuid }));
+    } else if (message.type === 'message') {
+      users.forEach(function each(user) {
+        if (user.ws != ws && user.ws.readyState == ws.OPEN) {
+          user.ws.send(JSON.stringify({ type: 'message', username: message.username, text: message.text }));
+        }
+      });
+    }
+  });
+});
 
 server.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
